dd483a4c390b1e7f7c709f44d62b558fe85f455b
geetheswar-v <geetheswar.edu@gmail.com>
2026-02-21 14:36:04 +0530

    Web: Added Theme to application and added sonner

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 })
