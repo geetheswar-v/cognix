7eee1a47990e2826b2696237694d08113603b15b
geetheswar-v <geetheswar.edu@gmail.com>
2026-02-22 06:29:08 +0530

    Web: Added auto gen API client using hey-api
    Co-authored-by: ManoharDarla2 <112201034@smail.iitpkd.ac.in>

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from sqlalchemy import text
 
 from app.api.endpoints.auth import router as auth_router
 from app.db.session import engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 
--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,6 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { ThemeProvider } from "@/components/theme-provider"
+import { client } from '@/client/client.gen'
+
+client.setConfig({
+  baseUrl: import.meta.env.VITE_API_URL,
+})
 
 export const Route = createRootRoute({
   component: () => (
